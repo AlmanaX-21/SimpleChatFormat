8f137b77d5457d29819535431366d952567a49f9
Lilly <46890129+RainbowDashLabs@users.noreply.github.com>
2023-08-17 14:18:21 +0200

    Replace StringBuilder with StringJoiner
    Refactored the string handling in the FormatsCommand class from StringBuilder to StringJoiner to simplify string concatenation. This update increases readability and enhances efficiency by reducing the number of newly created objects.

--- src/main/java/me/almana/simplechatformat/commands/FormatsCommand.java
+++ src/main/java/me/almana/simplechatformat/commands/FormatsCommand.java
@@ -17,7 +17,9 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
+import java.util.StringJoiner;
+import java.util.function.Predicate;
+import java.util.logging.Level;
 
 public class FormatsCommand implements TabExecutor {
 
@@ -43,13 +45,12 @@ public class FormatsCommand implements TabExecutor {
         if (args[0].equalsIgnoreCase("add")) {
 
             String groupOrPlayer = args[2];
-            StringBuilder builder = new StringBuilder();
-            String format = "";
+            StringJoiner builder = new StringJoiner(" ");
             for (int i = 3; i < args.length; i++) {
 
-                builder.append(args[i] + " ");
+                builder.add(args[i]);
             }
-            format = new String(builder);
+            String format = builder.toString();
             if (args[1].equalsIgnoreCase("group") && checkIfGroup(groupOrPlayer)) {
 
                 formatAdd(groupOrPlayer, format, sender);
